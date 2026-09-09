e3f06ec650e62b2334871baf6d56aa9c025a7d81
spider-calvin <calvindas7@gmail.com>
2026-09-09 17:53:12 +0530

    feat: wire up tanstack query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { queryClient } from './lib/queryClient'
 import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
